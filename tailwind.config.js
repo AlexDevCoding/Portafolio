ee3d67ed6af5a1451aa4884860b8e4a5146a10c4
Josue547 <henriquezjosue384@gmail.com>
2024-10-04 17:03:19 -0400

    asdasd

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}",
-    'node_modules/preline/dist/*.js',
+  content: ["./src/**/*.{html,js}"
   ],
   theme: {
     extend: {},
   },
-  plugins: [
-    require('preline/plugin'),
-  ],
+
 }
